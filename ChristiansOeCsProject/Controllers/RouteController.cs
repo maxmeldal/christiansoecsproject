9594bd7d74588d3701278822aa563832abbd1150
NicolaiOkkels <nico902t@stud.kea.dk>
2021-11-09 11:18:14 +0100

    added route controller

--- ChristiansOeCsProject/Controllers/RouteController.cs
+++ ChristiansOeCsProject/Controllers/RouteController.cs
@@ -1,7 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ChristiansOeCsProject.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ChristiansOeCsProject.Service;
+
 namespace ChristiansOeCsProject.Controllers
 {
-    public class RouteController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RouteController : ControllerBase
     {
-        
+        private readonly RouteService _routeService;
+
+        public RouteController(RouteService routeService)
+        {
+            _routeService = routeService;
+        }
+
+        [HttpGet]
+        public Task<ActionResult<IEnumerable<Route>>> GetRoutes()
+        {
+            return null;
+        }
+
+        // [HttpGet("{id:int}")]
+        // public async Task<ActionResult<Route>> GetRoute(int id)
+        // {
+        //     try
+        //     {
+        //         var result = await _routeService.ReadById(id);
+        //
+        //         if (result == null)
+        //         {
+        //             return NotFound();
+        //         }
+        //
+        //         return result;
+        //     }
+        //     catch (Exception)
+        //     {
+        //         return StatusCode(500, "Error");
+        //     }
+        // }
     }
 }
