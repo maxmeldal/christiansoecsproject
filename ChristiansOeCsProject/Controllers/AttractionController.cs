7f18d34b8bb30c446d87a091d8c0e759cca20e4f
NicolaiOkkels <nico902t@stud.kea.dk>
2021-11-10 10:17:00 +0100

    attraction controller fixed

--- ChristiansOeCsProject/Controllers/AttractionController.cs
+++ ChristiansOeCsProject/Controllers/AttractionController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace ChristiansOeCsProject.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Produces("application/json")]
     public class AttractionController : ControllerBase
     {
         private readonly AttractionService _attractionService;
@@ -18,21 +18,24 @@ namespace ChristiansOeCsProject.Controllers
         {
             _attractionService = attractionService;
         }
-
-        [HttpGet]
+        
+        //Http example:
+        //https://localhost:5001/api/attractions
+        [HttpGet("api/attactions")]
         public ActionResult GetAttractions()
         {
             try
             {
-                return Ok(_attractionService.ReadAll());
+                return Ok(_attractionService.ReadAll().Result);
             }
             catch (Exception)
             {
                 return new StatusCodeResult(StatusCodes.Status500InternalServerError);
             }
         }
-
-        [HttpGet("{id:int}")]
+        //Http example:
+        //https://localhost:5001/api/facilities/8d16be31-a7ff-45ff-907d-6cbcab477c40
+        [HttpGet("api/attraction/{id}")]
         public ActionResult GetAttraction(string id)
         {
             try
