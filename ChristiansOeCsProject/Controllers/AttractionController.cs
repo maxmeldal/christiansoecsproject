f070b609bc8a0164da2dfcbdf605832dbad1d78b
maxmeldal <70686398+maxmeldal@users.noreply.github.com>
2021-11-18 10:33:34 +0100

    azre

--- ChristiansOeCsProject/Controllers/AttractionController.cs
+++ ChristiansOeCsProject/Controllers/AttractionController.cs
@@ -20,7 +20,7 @@ namespace ChristiansOeCsProject.Controllers
         
         //Http example:
         //https://localhost:5001/api/attraction/attractions
-        [HttpGet("attractions")]
+        [HttpGet("attractionsss")]
         public ActionResult GetAttractions()
         {
             try
