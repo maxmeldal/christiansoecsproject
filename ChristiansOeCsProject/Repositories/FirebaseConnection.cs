ead96859ede1ef267e9121337411345235381ad5
maxmeldal <70686398+maxmeldal@users.noreply.github.com>
2021-11-22 13:33:42 +0100

    fixed so c# doesn't establish new connection to database every time a repository is instansiated but only once

--- ChristiansOeCsProject/Repositories/FirebaseConnection.cs
+++ ChristiansOeCsProject/Repositories/FirebaseConnection.cs
@@ -5,13 +5,18 @@ namespace ChristiansOeCsProject.Repositories
 {
     public class FirebaseConnection
     {
+        private static FirestoreDb _db;
         public static FirestoreDb GetConnection()
         {
+            if (_db != null)
+            {
+                return _db;
+            }
             string path = AppDomain.CurrentDomain.BaseDirectory + @"Christiansoe-Firebase.json";
             Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
 
-            FirestoreDb db = FirestoreDb.Create("christiansoe-4889c");
-            return db;
+            _db = FirestoreDb.Create("christiansoe-4889c");
+            return _db;
         }
     }
 }
